0d6ace20fb0cfbdfef575433bed29d27f262b1a7
martlandh <HarryMartland@Rentalcars.com>
2019-06-23 20:09:34 +0100

    Revert "removed service worker"
    This reverts commit 47018af1

--- app.js
+++ app.js
@@ -33,8 +33,15 @@ $(function () {
 
         navigator.serviceWorker.getRegistration().then(function(reg) {
 
-            return reg.showNotification(payload.notification.title,
-                payload.notification);
+
+            var notificationTitle = 'Background Message Title';
+            var notificationOptions = {
+                body: 'Background Message body.',
+                //icon: '/firebase-logo.png'
+            };
+
+            return reg.showNotification(notificationTitle,
+                notificationOptions);
         });
 
     });
@@ -80,3 +87,51 @@ $(function () {
         });
     })
 });
+
+if ('serviceWorker' in navigator) {
+    registerValidSW('/serviceWorker.js', {});
+}
+
+function registerValidSW(swUrl, config) {
+    navigator.serviceWorker
+        .register(swUrl)
+        .then(registration => {
+            registration.onupdatefound = () => {
+                const installingWorker = registration.installing;
+                if (installingWorker == null) {
+                    return;
+                }
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed') {
+                        if (navigator.serviceWorker.controller) {
+                            // At this point, the updated precached content has been fetched,
+                            // but the previous service worker will still serve the older
+                            // content until all client tabs are closed.
+                            console.log(
+                                'New content is available and will be used when all ' +
+                                'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                            );
+
+                            // Execute callback
+                            if (config && config.onUpdate) {
+                                config.onUpdate(registration);
+                            }
+                        } else {
+                            // At this point, everything has been precached.
+                            // It's the perfect time to display a
+                            // "Content is cached for offline use." message.
+                            console.log('Content is cached for offline use.');
+
+                            // Execute callback
+                            if (config && config.onSuccess) {
+                                config.onSuccess(registration);
+                            }
+                        }
+                    }
+                };
+            };
+        })
+        .catch(error => {
+            console.error('Error during service worker registration:', error);
+        });
+}
